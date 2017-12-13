2f69e3c6b7cffb9ba84e29f378e470d7f7f68fd4
Anuj Das <anuj.das@brigade.com>
2017-12-13 11:48:19 -0800

    Set up to read from REDIS_URL

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const mosca = require('mosca');
-const redis = require('redis');
+const Mosca = require('mosca');
+const { URL } = require('url');
 
 const port = parseInt(process.env.PORT) || 1883;
-const redisPort = parseInt(process.env.REDIS_PORT) || 6379;
+
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisSettings = ((url) => {
+  const { username, password, hostname, port } = new URL(url);
+  return { username, password, hostname, port };
+})(redisUrl);
 
 const moscaConfig = {
   port: port,
   backend: {
     type: 'redis',
-    redis: redis,
-    host: process.env.REDIS_HOST || 'localhost',
-    port: redisPort,
+    host: redisSettings.hostname,
+    port: redisSettings.port,
+    password: redisSettings.password,
     db: 0,
     return_buffers: true,
   }, persistence: {
-    factory: mosca.persistence.Redis,
-    host: process.env.REDIS_HOST || 'localhost',
-    port: redisPort,
+    factory: Mosca.persistence.Redis,
+    url: redisUrl,
   }
 };
 
-const server = new mosca.Server(moscaConfig);
+const server = new Mosca.Server(moscaConfig);
 
 server.on('ready', () => console.log('Mosca is listening'));
 server.on('clientConnected', client => console.log('client connected', client.id));
